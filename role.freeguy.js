52dc4b9cfb76ea4c2fb5f7f59265a5850801932c
Leman FENG <flm8620@gmail.com>
2019-12-08 19:43:25 +0800

    freeguy attack enemy

--- role.freeguy.js
+++ role.freeguy.js
@@ -17,9 +17,21 @@ var roleBuilder = {
         // debug(`ok = ${ok}`)
         // return;
 
-        let d = new RoomPosition(12, 43, 'W9S3');
+        //let d = new RoomPosition(12, 43, 'W9S3');
         //let d = new RoomPosition(23, 3, 'W9S5');
-        //let d = new RoomPosition(12, 5, 'W9S4');
+        const enemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (enemy) {
+            let p = enemy.pos;
+            if(!creep.pos.isNearTo(p)){
+                creep.moveTo(p);
+            } else {
+                creep.attack(enemy);
+            }
+            if(creep.pos.inRangeTo(p)){
+                creep.rangedAttack(enemy);
+            }
+        }
+        let d = new RoomPosition(10, 6, 'W9S4');
         dd.set_pos_as_destination(creep, d);
         if (!dd.has_destination(creep)) {
             debug(`set destination to ${d}`);
